bc718c26261b7bc46337cd2ec069c8f4419c1790
imorenoma <i.morenoma@alumnos.urjc.es>
2021-11-05 17:59:08 -0400

    Define characters, enemies functions

--- practica1/main.py
+++ practica1/main.py
@@ -1,35 +1,90 @@
 #!/bin/python3
+
+
 import sys
+from game import Person
+import random
+
+
+def character_selection(characters):
+
+    game_characters = {1: Person(25, 9), 2: Person(40, 10), 3: Person(20, 6), 4: Person(30, 6)}
+
+    play_character1 = game_characters.get(characters[0])
+    play_character2 = game_characters.get(characters[1])
+
+    return [play_character1.hp, play_character2.hp]
+
+
+def enemy_spotted(level):
+
+    partial = Person(20, 6)
+    final_exam = Person(40, 12)
+    theorical_class = Person(8, 4 + level)
+    teacher = Person(15, 7)
+
+    if teacher.dmg == 7:
+        teacher.dmg = 14
+
+    enemies = [partial, final_exam, theorical_class, teacher]
+
+    if level <= 2:
+        enemies.remove(final_exam)
+        return enemies
+    if level >= 3:
+        random_number = random.randint(0, 3)
+        print("los malosos")
+        return [enemies[random_number].hp, enemies[random_number].dmg]
+
 
 
 def type_players():
-    print("Choose Characters: " + "\n")
-    print("1.The_bookworn -> Stats: 25 HP and 9 DMG")
-    print("2.The_worker -> Stats: 40 HP and 10 DMG")
-    print("3.The_whatsapper -> Stats: 20 HP and 6 DMG")
-    print("4.The_procrastinator-> Stats: 30 HP and 6 DMG")
+
+    print("Choose Characters: ")
+    print("***************************" + "\n")
+
+    option_1 = "1.The_bookworn -> Stats: 25 HP and 9 DMG"
+    option_2 = "2.The_worker -> Stats: 40 HP and 10 DMG"
+    option_3 = "3.The_whatsapper -> Stats: 20 HP and 6 DMG"
+    option_4 = "4.The_procrastinator-> Stats: 30 HP and 6 DMG"
+
+    return option_1 + "\n" + option_2 + "\n" + option_3 + "\n" + option_4 + "\n"
 
 
 def main():
+    try:
+        if len(sys.argv) == 3:
+            if (sys.argv[1] == "-s") and (sys.argv[2] == "5"):
+                print("Partida 2 jugadores con 5 niveles")
+                print(type_players())
 
-    if len(sys.argv) == 3:
-        if (sys.argv[1] == "-s") and (sys.argv[2] == "5"):
-            print("Partida 2 jugadores con 5 niveles")
+                char_election_1 = int(input('selec first character: '))
+                char_election_2 = int(input("select second character: "))
+
+                characters = [char_election_1, char_election_2]
+
+                print(characters[0], characters[1])
+                print(character_selection(characters))
+
+                print(enemy_spotted(5))
+
+            if (sys.argv[1] == "-f") and (sys.argv[2] == "saved"):
+                print(sys.argv[2] + " Nombre fichero incorrecto")
+
+        if len(sys.argv) == 4:
+            if (sys.argv[1] == "--file=saved.txt") and (sys.argv[2] == "-s") and (sys.argv[3] == "5"):
+                print("Relanzamos partida guardada en saved.txt teniendo en cuenta el numero de niveles")
+
+        if (len(sys.argv) == 2) and (sys.argv[1] == "--file=saved.txt"):
+            print("Relanzamos la partida guardada en saved.txt")
+
+        if (len(sys.argv) == 1) and (sys.argv[0] == "main.py"):
             type_players()
 
-
-        if (sys.argv[1] == "-f") and (sys.argv[2] == "saved"):
-            print(sys.argv[2] + " Nombre fichero incorrecto")
-
-    if len(sys.argv) == 4:
-        if (sys.argv[1] == "--file=saved.txt") and (sys.argv[2] == "-s") and (sys.argv[3] == "5"):
-            print("Relanzamos partida guardada en saved.txt teniendo en cuenta el numero de niveles")
-
-    if (len(sys.argv) == 2) and (sys.argv[1] == "--file=saved.txt"):
-        print("Relanzamos la partida guardada en saved.txt")
-
-    if (len(sys.argv) == 1) and (sys.argv[0] == "main.py"):
-        type_players()
+    except TypeError:
+        print("fatal error")
+    except KeyboardInterrupt:
+        print("Exit program")
 
 
 if __name__ == '__main__':
